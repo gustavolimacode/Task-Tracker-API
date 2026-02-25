89c1dbff4339b56e5ceb6459389f258e7b43fce2
Gustavo Lima <gustavoblima306@gmail.com>
2026-02-24 21:34:39 -0300

    feat: implement task GET and POST endpoints

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter
+from app.schemas.task import TaskCreate, Task
+import app.database as db
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @router.get("/")
 def list_tasks():
-    return {"message": "list_tasks has not yet been implemented"}
+    return db.tasks
 
-@router.post("/")
-def create_task():
-    return {"message": "create_task has not yet been implemented"}
+@router.post("/", response_model=Task)
+def create_task(data: TaskCreate):
+    new_task = {
+        "id": db.next_id,
+        "title": data.title,
+        "done": False
+    }
+    db.next_id += 1
+    db.tasks.append(new_task)
+    return new_task
